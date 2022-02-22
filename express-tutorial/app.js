feec7df9d773406fc1cef5a4c8b849fc536141ed
Jundi <indana6969@gmail.com>
2022-02-22 19:23:42 +0700

    send a response

--- express-tutorial/app.js
+++ express-tutorial/app.js
@@ -7,13 +7,13 @@ const app = express()
 // use() allows us to add a new middleware function
 app.use((req, res, next) => {
     console.log("In the middleware!")
-    // next to go to another middleware
+    // next allows us to go to the next/another middleware
     next()
 })
 
 app.use((req, res, next) => {
     console.log("In another middleware!")
-
+    res.send('<h1>Hello from Express</h1>')
 })
 
 // create server
