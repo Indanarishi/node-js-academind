2d8cbdf5b5ce18bc48fed0704d6bed19ad6be2d9
Jundi <indana6969@gmail.com>
2022-02-22 19:34:09 +0700

    handling routes

--- express-tutorial/app.js
+++ express-tutorial/app.js
@@ -5,18 +5,15 @@ const express = require('express')
 const app = express()
 
 // use() allows us to add a new middleware function
-app.use((req, res, next) => {
-    console.log("In the middleware!")
-    // next allows us to go to the next/another middleware
-    next()
+app.use('/add-product', (req, res, next) => {
+    console.log("In another middleware!")
+    res.send('<h1>Add Product Page</h1>')
 })
 
-app.use((req, res, next) => {
+app.use('/', (req, res, next) => {
     console.log("In another middleware!")
     res.send('<h1>Hello from Express</h1>')
 })
 
 // create server
-const server = http.createServer(app)
-
-server.listen(3000)
+app.listen(3000)
