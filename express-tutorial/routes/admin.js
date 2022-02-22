f87bee256098037b9cd2332fab1db808aea600d8
Jundi <indana6969@gmail.com>
2022-02-22 21:02:17 +0700

    create a helper function for navigation

--- express-tutorial/routes/admin.js
+++ express-tutorial/routes/admin.js
@@ -1,12 +1,15 @@
 const path = require('path')
 const express = require('express')
 
+const rootDir = require('../util/path')
+
 const router = express.Router()
+
 
 // use() allows us to add a new middleware function
 // /admin/add-product => GET
 router.get('/add-product', (req, res, next) => {
-    res.sendFile(path.join(__dirname, '../', 'views', 'add-product.html'))
+    res.sendFile(path.join(rootDir, 'views', 'add-product.html'))
 })
 
 // /admin/add-product => POST
