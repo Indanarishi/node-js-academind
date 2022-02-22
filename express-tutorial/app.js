fb269c93fe7d4a29f4cd939f4ad10b051d8b9d75
Jundi <indana6969@gmail.com>
2022-02-22 19:12:55 +0700

    add a middleware with use, and next function

--- express-tutorial/app.js
+++ express-tutorial/app.js
@@ -4,6 +4,18 @@ const express = require('express')
 // init express
 const app = express()
 
+// use() allows us to add a new middleware function
+app.use((req, res, next) => {
+    console.log("In the middleware!")
+    // next to go to another middleware
+    next()
+})
+
+app.use((req, res, next) => {
+    console.log("In another middleware!")
+
+})
+
 // create server
 const server = http.createServer(app)
 
