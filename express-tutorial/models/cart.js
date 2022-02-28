bccd8c49fb2cf36adbdcf3076da3eb7e9e3164e2
Jundi <indana6969@gmail.com>
2022-02-28 17:46:41 +0700

    delete cart items and total prices

--- express-tutorial/models/cart.js
+++ express-tutorial/models/cart.js
@@ -8,9 +8,11 @@ module.exports = class Cart {
         // Fetch the previous cart
         fs.readFile(p, (err, fileContent) => {
             let cart = {products: [], totalPrice: 0}
-            if (!err) {
-                cart = JSON.parse(fileContent)
+            if (err) {
+                return
             }
+            // console.log(JSON.parse(fileContent))
+            cart = JSON.parse(fileContent)
 
             // Analyze the cart => Find existing product
             const existingProductIndex = cart.products.findIndex(product => product.id === id)
@@ -31,6 +33,22 @@ module.exports = class Cart {
 
             cart.totalPrice += +productPrice
             fs.writeFile(p, JSON.stringify(cart), (err) => {
+                console.log(err)
+            })
+        })
+    }
+
+    static deleteProduct(id, productPrice) {
+        fs.readFile(p, (err, fileContent) => {
+            if (err) {
+                return
+            }
+            const updatedCart = { ...JSON.parse(fileContent) }
+            const product = updatedCart.products.find(prod => prod.id === id)
+            const productQty = product.qty
+            updatedCart.products = updatedCart.products.filter(prod => prod.id !== id)
+            updatedCart.totalPrice = updatedCart.totalPrice - productPrice * productQty
+            fs.writeFile(p, JSON.stringify(updatedCart), (err) => {
                 console.log(err)
             })
         })
