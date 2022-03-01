5181192694ec199bc528da0960902a4ab996386a
Jundi <indana6969@gmail.com>
2022-03-01 17:06:45 +0700

    fetching products from database

--- express-tutorial/controllers/shop.js
+++ express-tutorial/controllers/shop.js
@@ -23,13 +23,15 @@ exports.getProduct = (req, res, next) => {
 }
 
 exports.getIndex = (req, res, next) => {
-    Product.fetchAll((products) => {
-        res.render('shop/index', {
-            prods: products, 
-            docTitle: 'Shop', 
-            path: '/'
+    Product.fetchAll()
+        .then(([rows, fieldData]) => {
+            res.render('shop/index', {
+                prods: rows, 
+                docTitle: 'Shop', 
+                path: '/'
+            })
         })
-    })
+        .catch(err => console.log(err))
 }
 
 exports.getCart = (req, res, next) => {
