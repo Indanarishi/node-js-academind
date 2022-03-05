4b816328d538ee697f8d5a2713509f62c526df62
Indana <indana6969@gmail.com>
2022-03-05 20:08:48 +0700

    updating products

--- express-tutorial/controllers/admin.js
+++ express-tutorial/controllers/admin.js
@@ -29,17 +29,19 @@ exports.getEditProduct = (req, res, next) => {
     if (!editMode) {
         return res.redirect('/')
     }
-    Product.findById(prodId, (product) => {
-        if (!product) {
-            return res.redirect('/')
-        }
-        res.render('admin/edit-product', {
-            docTitle: 'Edit Product', 
-            path: '/admin/edit-product',
-            editing: editMode,
-            product
+    Product.findByPk(prodId)
+        .then(product => {
+            if (!product) {
+                return res.redirect('/')
+            }
+            res.render('admin/edit-product', {
+                docTitle: 'Edit Product',
+                path: '/admin/edit-product',
+                editing: editMode,
+                product: product
+            })
         })
-    })
+        .catch(err => console.log(err))
 }
 
 exports.postEditProduct = (req, res, next) => {
@@ -48,9 +50,21 @@ exports.postEditProduct = (req, res, next) => {
     const updatedPrice = req.body.price
     const updatedImageUrl = req.body.imageUrl
     const updatedDesc = req.body.desc
-    const updatedProduct = new Product(prodId, updatedTitle, updatedImageUrl, updatedPrice, updatedDesc)
-    updatedProduct.save()
-    res.redirect('/admin/products')
+    
+    Product.findByPk(prodId)
+        .then(product => {
+            product.title = updatedTitle;
+            product.price = updatedPrice;
+            product.desc = updatedDesc;
+            product.imageUrl = updatedImageUrl;
+            return product.save()
+        })
+        .then(result => {
+            console.log('Updated Product')
+            res.redirect('/admin/products')
+        })
+        .catch(err => console.log(err))
+
 }
 
 exports.getProducts = (req, res, next) => {
