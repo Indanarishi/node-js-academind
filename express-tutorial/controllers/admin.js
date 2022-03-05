8247298f503eabe071274ad53294b39eafc350eb
Indana <indana6969@gmail.com>
2022-03-05 20:43:35 +0700

    delete products

--- express-tutorial/controllers/admin.js
+++ express-tutorial/controllers/admin.js
@@ -17,8 +17,9 @@ exports.postAddProduct = (req, res, next) => {
         imageUrl,
         desc
     })
-    .then(res => {
+    .then(result => {
         console.log('Product Created')
+        res.redirect('/admin/products')
     })
     .catch(err => console.log(err))
 }
@@ -83,6 +84,13 @@ exports.getProducts = (req, res, next) => {
 
 exports.postDeleteProduct = (req, res, next) => {
     const prodId = req.body.productId
-    Product.deleteById(prodId)
-    res.redirect('/admin/products')
+    Product.findByPk(prodId)
+        .then(product => {
+            return product.destroy()
+        })
+        .then(result => {
+            console.log('Product Destroyed')
+            res.redirect('/admin/products')
+        })
+        .catch(err => console.log(err))
 }
